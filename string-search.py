8741467359ff61a523626fb928256f7e48bbe22d
e <info@e-anderson.dev>
2022-06-29 19:45:07 -0400

    needle n haystack

---
+++ string-search.py
@@ -0,0 +1,50 @@
+import timeit
+
+def string_search(substr, str):
+  sub_len = len(substr)
+
+  idx = 0
+  lp = 0
+  in_substr = False # first char in substring
+
+  while idx < len(str) - (sub_len - lp):
+    if str[idx] == substr[lp]:
+      if lp == sub_len - 1:
+        return idx - lp
+      if lp > 0 and substr[0] == str[idx]:
+        in_substr = True
+      lp += 1
+    else:
+      if in_substr:
+        idx -= lp
+        in_substr = False
+      lp = 0
+    idx += 1
+
+  return -1
+
+def indexOf(needle, haystack):
+  for h_idx in range(len(haystack) - len(needle)):
+    for n_idx in range(len(needle)):
+      if haystack[h_idx + n_idx] != needle[n_idx]:
+        break
+      if n_idx + 1 == len(needle):
+        return h_idx
+
+  return -1
+
+start = timeit.default_timer()
+print(string_search('or', 'hello world'), 'should equal 7') # should return 7
+print(string_search('hello world', 'or'), 'should equal -1') # should return -1
+print(string_search('howdy', 'hello world'), 'should equal -1') # should return -1
+print(string_search('oox', 'ooboxoooxo'), 'should equal 6') # should return 6)
+stop = timeit.default_timer()
+print('Time: ', stop - start)
+
+start = timeit.default_timer()
+print(indexOf('or', 'hello world'), 'should equal 7') # should return 7
+print(indexOf('hello world', 'or'), 'should equal -1') # should return -1
+print(indexOf('howdy', 'hello world'), 'should equal -1') # should return -1
+print(indexOf('oox', 'ooboxoooxo'), 'should equal 6') # should return 6)
+stop = timeit.default_timer()
+print('Time: ', stop - start)
