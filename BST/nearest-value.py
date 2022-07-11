ea5fc2cf1a91370f050029a21d2bbc90a7aef533
JSimple <josephinemayasimple@gmail.com>
2022-07-11 17:21:18 -0400

    add: BST nearest value

---
+++ BST/nearest-value.py
@@ -0,0 +1,32 @@
+# given a tree and a target 
+# return the value in your tree that's closest to the target
+
+def findClosestValueInBst(tree, target):
+    diff = tree.value - target 
+
+    if tree.value == target:
+        return target
+    
+    if diff > 0: # current val is bigger than target
+        # base case:
+        if tree.left == None:
+            return tree.value
+        #recursive:
+        left_tree_closest = findClosestValueInBst(tree.left, target)
+        return tree.value if diff < abs(left_tree_closest - target) else left_tree_closest
+
+    if diff < 0: # current val is smaller than target
+        # base case:    
+        if tree.right == None:
+            return tree.value
+        #recursive:
+        right_tree_closest = findClosestValueInBst(tree.right, target)
+        return tree.value if abs(diff) < abs(right_tree_closest - target) else right_tree_closest
+        #return your value or previous return value depending on which is closer to target
+
+# This is the class of the input tree. Do not edit.
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
