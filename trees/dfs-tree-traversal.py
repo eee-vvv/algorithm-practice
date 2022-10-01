1167f0bfb284304b0f3de447f03de58dd9c3c979
JSimple <josephinemayasimple@gmail.com>
2022-10-01 01:09:43 -0400

    added test file and edited dfs

--- trees/dfs-tree-traversal.py
+++ trees/dfs-tree-traversal.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 class Node:
     def __init__(self, name):
         self.children = []
@@ -7,24 +9,39 @@ class Node:
         self.children.append(Node(name))
         return self
 
-    ### ITTERATIVE
-    def _processNode(self, output: list, q: list):
-        output.append(q.pop(0).name)
-        q = self.children + q
-        return q, output
-
-    def i_depthFirstSearch(self):
-        output = []
-        q = [self]
-        while q:
-            self = q[0]
-            q, output = self._processNode(output,q)
-        return output
+    def i_dfs(self):
+        stack = [self]
+        arr = []
+        while stack:
+            current = stack.pop()
+            arr.append(current.name)
+            stack += (reversed(current.children))
+        return arr
     
-    ### RECURSIVE
-    def depthFirstSearch(self, array: list):
-        array.append(self.name)
+    def r_dfs(self,arr):
+        arr.append(self.name)
         for c in self.children:
-            c.depthFirstSearch(array)
-        return array
+            c.r_dfs(arr)
+        return arr
+            
+    
+    # ### ITTERATIVE
+    # def _processNode(self, output: list, q: list):
+    #     output.append(q.pop(0).name)
+    #     q = self.children + q
+    #     return q, output
 
+    # def i_depthFirstSearch(self):
+    #     output = []
+    #     q = [self]
+    #     while q:
+    #         self = q[0]
+    #         q, output = self._processNode(output,q)
+    #     return output
+    
+    # ### RECURSIVE
+    # def depthFirstSearch(self, array: list):
+    #     array.append(self.name)
+    #     for c in self.children:
+    #         c.depthFirstSearch(array)
+    #     return array
