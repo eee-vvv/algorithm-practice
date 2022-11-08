5367555dd3f27d35a224d283a22b2fe519499d2f
JSimple <josephinemayasimple@gmail.com>
2022-11-08 10:59:45 -0500

    two pointer stuff

---
+++ two_pointers/two_sum_ordered.py
@@ -0,0 +1,25 @@
+'''
+Given a 1-indexed array of integers numbers 
+that is already sorted in non-decreasing order, 
+find two numbers such that they add up to a specific target number. 
+Let these two numbers be numbers[index1] and numbers[index2] 
+where 1 <= index1 < index2 <= numbers.length.
+Return the indices of the two numbers, index1 and index2, 
+added by one as an integer array [index1, index2] of length 2.
+
+You may not use the same element twice.
+
+Your solution must use only constant extra space.
+'''
+def twoSum(numbers: list[int], target: int) -> list[int]:
+
+    l, r = 0, len(numbers)-1
+
+    while l < r:
+        if target - numbers[r] < numbers[l]:
+            r -= 1
+        elif target - numbers[r] > numbers[l]:
+            l += 1
+        else:        
+            return([l+1,r+1])
+    return -1
